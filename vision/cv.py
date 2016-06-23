1a357386555bc045e594f9286f56705345e4fb3a
IngaPr <usericca1127@gmail.com>
2016-06-23 10:34:19 +0300

    Vision

--- vision/cv.py
+++ vision/cv.py
@@ -7,31 +7,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io
 
-image_file = io.imread("Sprites\healthFace.png")
 image_file2 = io.imread("Sprites\Test.png")
-img1 = rgb2gray(image_file)
 img2 = rgb2gray(image_file2)
 
-keypoints1 = corner_peaks(corner_harris(img1), min_distance=1)
-keypoints2 = corner_peaks(corner_harris(img2), min_distance=1)
+image_file = io.imread("Sprites\healthFace.png")
+img1 = rgb2gray(image_file)
 
-extractor = BRIEF()
+image_file3 = io.impread("Sprites\Enemie.PNG")
+img3 = rgbgray(image_file3)
 
-extractor.extract(img1, keypoints1)
-keypoints1 = keypoints1[extractor.mask]
-descriptors1 = extractor.descriptors
+image_file4 = io.imread("Sprites\Armor.png")
+img4 = rgb2gray(image_file4)
 
-extractor.extract(img2, keypoints2)
-keypoints2 = keypoints2[extractor.mask]
-descriptors2 = extractor.descriptors
+image_file5 = io.impread("Sprites\Shells.PNG")
+img5 = rgb2gray(image_file5)
 
-matches12 = match_descriptors(descriptors1, descriptors2, cross_check=True)
+image_file6 = io.impread("Sprites\Weapon.PNG")
+img6 = rgb2gray(image_file6)
 
-fig, ax = plt.subplots(nrows=2, ncols=1)
+image_arr = [img1, img3, img4, img5, img6]
 
-plt.gray()
+for i in range (6):
+    keypoints1 = corner_peaks(corner_harris(image_arr[i]), min_distance=1)
+    keypoints2 = corner_peaks(corner_harris(img2), min_distance=1)
 
-plot_matches(ax[0], img1, img2, keypoints1, keypoints2, matches12)
-ax[0].axis('off')
+    extractor = BRIEF()
+
+    extractor.extract(image_arr[i], keypoints1)
+    keypoints1 = keypoints1[extractor.mask]
+    descriptors1 = extractor.descriptors
+
+    extractor.extract(img2, keypoints2)
+    keypoints2 = keypoints2[extractor.mask]
+    descriptors2 = extractor.descriptors
+
+    matches12 = match_descriptors(descriptors1, descriptors2, cross_check=True)
+
+    fig, ax = plt.subplots(nrows=2, ncols=1)
+
+    plt.gray()
+
+    plot_matches(ax[0], image_arr[i], img2, keypoints1, keypoints2, matches12)
+    ax[0].axis('off')
 
 plt.show()
